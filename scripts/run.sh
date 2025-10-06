9d326ca94e0ca7ea3bac072b06b18799f28b9c45
Yahwista Salomo <yahwistasalomo@gmail.com>
2025-10-06 09:57:06 +0800

    Enable touchpad tapping and two-finger tap as right-click

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-xrdb merge ~/.Xresources 
-xbacklight -set 10 &
-feh --bg-fill ~/Pictures/wall/gruv.png &
+xrdb merge ~/.Xresources
+nitrogen --restore &
 xset r rate 200 50 &
+xinput set-prop 9 "libinput Tapping Enabled" 1 &
+xinput set-prop 9 "libinput Tapping Button Mapping Enabled" 0 1 &
+sxhkd -c ~/.config/chadwm/sxhkd/sxhkdrc &
 picom &
 
 dash ~/.config/chadwm/scripts/bar.sh &
